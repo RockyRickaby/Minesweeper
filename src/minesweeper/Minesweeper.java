cfa2ecee77d46f155137ef74e0d4933616fec336
Maurício Alcântara da Luz <iamthefreeman@outlook.com.br>
2023-08-10 17:47:52 -0300

    now the windows is resizeable. changed a little how the buttons and the grid are shown on screen

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -3,9 +3,10 @@ package minesweeper;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
@@ -18,49 +19,52 @@ import java.awt.event.ActionListener;
 @SuppressWarnings("serial")
 public class Minesweeper extends JFrame implements ActionListener {
     private Grid grid;
-    private Difficulties dif;
+    private Difficulties difficulty;
     public Minesweeper() {
         super();
 		
-        Difficulties[] itens = {Difficulties.EASY,
+        Difficulties[] difficulties = {Difficulties.EASY,
                                 Difficulties.INTERMEDIATE,
                                 Difficulties.ADVANCED};
-        dif = (Difficulties) JOptionPane.showInputDialog(this, "Please, choose a difficulty", "Difficulties", JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]);
+        this.difficulty = (Difficulties) JOptionPane.showInputDialog(this, "Please, choose a difficulty", "Difficulties", JOptionPane.INFORMATION_MESSAGE, null, difficulties, difficulties[0]);
         
-        if (dif == null) {
+        if (this.difficulty == null) {
         	System.exit(1);
         }
 		
-        JPanel restart = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JButton rest = new JButton("restart");
-        rest.addActionListener(this);
-        restart.add(rest);
+        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        
+        JButton restart = new JButton("restart");
+        restart.addActionListener(this);
+        buttons.add(restart);
 	
         JButton mark = new JButton("mark");
         mark.addActionListener(this);
-        restart.add(mark);
+        buttons.add(mark);
 		
-        grid = Grid.getInstance(dif);
+        grid = Grid.getInstance(this.difficulty);
         JPanel gridContainer = new JPanel(new GridBagLayout());
         gridContainer.add(grid);
         JScrollPane scrollGrid = new JScrollPane(gridContainer);
 		
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.add(restart);
+        panel.setLayout(new BorderLayout());
+        panel.add(buttons, BorderLayout.PAGE_START);
         panel.add(scrollGrid);
-        this.setTitle("Minesweeper");
-        this.getContentPane().add(panel);
+        
+        this.getContentPane().add(panel, BorderLayout.CENTER);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        double monitorHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-        double monitorWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-        if (monitorHeight >= 1080 || dif == Difficulties.EASY || (dif == Difficulties.ADVANCED && monitorWidth > 1024)) {
+        this.setTitle("Minesweeper");
+        
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double monitorHeight = screenSize.getHeight();
+        double monitorWidth = screenSize.getWidth();
+        if (monitorHeight >= 1080 || this.difficulty == Difficulties.EASY || (this.difficulty == Difficulties.ADVANCED && monitorWidth > 1024)) {
             this.pack();
         } else {
             this.setSize(676, 600);
         }
-        this.setResizable(false);
+        //this.setResizable(false);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
     }
@@ -73,12 +77,14 @@ public class Minesweeper extends JFrame implements ActionListener {
         	System.out.println("Restarting the game...");
             this.grid.reset();
         }
-        if (buttonText.equals("mark")) {
+        if (buttonText.equals("mark") || buttonText.equals("marking")) {
             this.grid.toggleMarkMode();
             if (this.grid.inMarkMode()) {
             	System.out.println("Mark mode toggled on");
+            	button.setText("marking");
             } else {
             	System.out.println("Mark mode toggled off");
+            	button.setText("mark");
             }
         }
     }
